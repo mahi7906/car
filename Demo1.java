5b395f51a75fb57562dce52621887710a9f2be00
mahi7906 <125970036+mahi7906@users.noreply.github.com>
2023-03-05 11:25:34 +0530

    ok

---
+++ Demo1.java
@@ -0,0 +1,31 @@
+class Demo1{
+	public static void main(String ar[]){
+		
+		int a;
+		a=10;
+		System.out.println("value of a="+a);
+		System.out.println("Welcome to java..!!!");
+		
+		short s=67;
+		System.out.println("value of ="+s);
+		
+		long mob=6798788789L;
+		System.out.println("value of ="+mob);
+		
+		byte b=127;
+		System.out.println("value of ="+b);
+		
+		float f=67.8f;
+		System.out.println("value of ="+f);
+		
+		double d=67.89d;
+		System.out.println("value of ="+d);
+		
+		char c='A';
+		System.out.println("value of ="+c);
+		
+		boolean bl=true;
+		System.out.println("value of ="+bl);
+		
+	}
+}
